49c544cbdabbd931a9d62dee8839f4e51de147ee
simonweisenhorn <simonweisenhorn@gmail.com>
2023-06-25 20:10:35 -0400

    Visual progress update. Just need to comment code after this hopefully.

--- renderScript.R
+++ renderScript.R
@@ -1,3 +1,6 @@
-rmarkdown::render(input = 'Final.Rmd',
+rmarkdown::render(input = 'index.Rmd',
                   output_format = "github_document",
-                  output_options = list(html_preview=FALSE))
+                  output_file = 'README.md',
+                  output_options = list(html_preview=FALSE,
+                                        toc = TRUE,
+                                        toc_depth = 2))
